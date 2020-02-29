d39f0341b4f7d4cdad4ac4411ad1128dd0664c85
Jungwoo An <o1o9814@naver.com>
2020-02-29 13:56:34 +0900

    chore: 모노 레포 구성

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,39 @@
+import fs from 'fs'
+import path from 'path'
+
 import typescript from 'rollup-plugin-typescript2'
 
-const FILENAME = 'dist/webpack-differential-loading'
-
-export default {
-  input: 'lib/index.ts',
-  output: [
-    {
-      file: `${FILENAME}.es.js`,
-      format: 'es',
-      sourcemap: true
-    },
-    {
-      file: `${FILENAME}.js`,
-      format: 'cjs',
-      sourcemap: true
+const packages = (fs.readdirSync(path.resolve('./packages')) || []).reduce(
+  (result, item) => {
+    const fullPath = path.resolve(`./packages/${item}`)
+    const isDirectory = fs.statSync(fullPath).isDirectory()
+    if (!isDirectory) {
+      return result
     }
-  ],
-  plugins: [typescript()]
-}
+
+    result.push(fullPath)
+
+    return result
+  },
+  []
+)
+
+export default packages.map(dir => {
+  const FILENAME = `${dir}/dist/${path.basename(dir)}`
+  return {
+    input: `${dir}/lib/index.ts`,
+    output: [
+      {
+        file: `${FILENAME}.es.js`,
+        format: 'es',
+        sourcemap: true
+      },
+      {
+        file: `${FILENAME}.js`,
+        format: 'cjs',
+        sourcemap: true
+      }
+    ],
+    plugins: [typescript()]
+  }
+})
