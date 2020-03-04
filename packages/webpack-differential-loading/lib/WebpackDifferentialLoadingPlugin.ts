eead2bfb9cb3c4d615fedccf80a2bf84fda217d7
Jungwoo An <o1o9814@naver.com>
2020-03-05 00:48:41 +0900

    feat: WebpackDifferentialLoadingPlugin 추가
    주어진 mode 에 따라 스크립트 태그를 수정 함 (type=module or nomodule)

---
+++ packages/webpack-differential-loading/lib/WebpackDifferentialLoadingPlugin.ts
@@ -0,0 +1,67 @@
+import { Plugin, Compiler } from 'webpack'
+
+import * as HtmlWebpackPlugin from 'html-webpack-plugin'
+
+import { BuildType, IHtmlTag } from './types'
+import { recentlyAssetTags } from './store'
+import { updateHtmlTag } from './utils'
+
+class WebpackDifferentialLoadingPlugin implements Plugin {
+  private _mode: BuildType
+
+  constructor(mode: BuildType) {
+    this._mode = mode
+  }
+
+  apply(compiler: Compiler) {
+    compiler.hooks.compilation.tap(this.constructor.name, compilation => {
+      const hooks = compilation.hooks as HtmlWebpackPlugin.Hooks
+      hooks.htmlWebpackPluginAlterAssetTags.tapPromise(
+        this.constructor.name,
+        this.updateAssetTags
+      )
+    })
+  }
+
+  updateTag = (tag: IHtmlTag) => {
+    if (tag.tagName !== 'script') {
+      return tag
+    }
+
+    return updateHtmlTag({ mode: this._mode, tag })
+  }
+
+  updateAssetTags = async (htmlPluginData: {
+    head: IHtmlTag[]
+    body: IHtmlTag[]
+    outputName: string
+    plugin: HtmlWebpackPlugin
+  }) => {
+    const hasPreviousBuildAssetTags =
+      recentlyAssetTags.head.length > 0 || recentlyAssetTags.body.length > 0
+
+    if (!hasPreviousBuildAssetTags) {
+      // first build
+      recentlyAssetTags.head = htmlPluginData.head.map(this.updateTag)
+      recentlyAssetTags.body = htmlPluginData.body.map(this.updateTag)
+      return htmlPluginData
+    }
+
+    // last build
+    const result = {
+      ...htmlPluginData,
+      head: htmlPluginData.head.map(this.updateTag),
+      body: htmlPluginData.body.map(this.updateTag)
+    }
+
+    const merge =
+      this._mode === 'legacy' ? Array.prototype.unshift : Array.prototype.push
+
+    merge.apply(result.head, recentlyAssetTags.head)
+    merge.apply(result.body, recentlyAssetTags.body)
+
+    return result
+  }
+}
+
+export default WebpackDifferentialLoadingPlugin
--- packages/webpack-differential-loading/lib/index.ts
+++ packages/webpack-differential-loading/lib/index.ts
@@ -1,16 +1,20 @@
-import WebpackDifferentialLoading from './WebpackDifferentialLoading'
+import { Configuration } from 'webpack'
 
 import { ConfigurationFunctionType } from './types'
 
-export function setup(makeConfiguration: ConfigurationFunctionType) {
+import WebpackDifferentialLoadingPlugin from './WebpackDifferentialLoadingPlugin'
+
+export function setup(
+  makeConfiguration: ConfigurationFunctionType
+): Configuration[] {
   const legacyConfig = makeConfiguration('legacy')
   const modernConfig = makeConfiguration('modern')
 
   const legacyPlugins = legacyConfig.plugins || (legacyConfig.plugins = [])
   const modernPlugins = modernConfig.plugins || (modernConfig.plugins = [])
 
-  legacyPlugins.push(new WebpackDifferentialLoading('legacy'))
-  modernPlugins.push(new WebpackDifferentialLoading('modern'))
+  legacyPlugins.push(new WebpackDifferentialLoadingPlugin('legacy'))
+  modernPlugins.push(new WebpackDifferentialLoadingPlugin('modern'))
 
   return [legacyConfig, modernConfig]
 }
